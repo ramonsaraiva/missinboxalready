12d371b3b4769be72d1ad4f2a7912db33a41c2e8
Ramon Saraiva <ramonsaraiva@gmail.com>
2019-03-22 17:41:41 -0300

    Add initial misser POST logic

--- missers/views.py
+++ missers/views.py
@@ -2,6 +2,8 @@ from rest_framework import (
     mixins,
     viewsets,
 )
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from django.views.generic import ListView
 
@@ -17,3 +19,7 @@ class MissersView(ListView):
 class MissersViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = Misser.objects.all()
     serializer_class = MisserSerializer
+
+    @action(detail=False)
+    def count(self, request):
+        return Response({'count': self.get_queryset().count()})
--- static/main.js
+++ static/main.js
@@ -1,1 +1,48 @@
-let counter = document.getElementsByClassName("js-counter");
+let COUNTER_SELECTOR = 'js-counter';
+let counter_element = document.getElementsByClassName(COUNTER_SELECTOR)[0];
+
+let CTA_SELECTOR = 'js-cta';
+let cta_element = document.getElementsByClassName(CTA_SELECTOR)[0];
+
+let polling_xhttp = new XMLHttpRequest();
+let misser_xhttp = new XMLHttpRequest();
+
+let poll_count = function() {
+    polling_xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            response = JSON.parse(this.responseText);
+            counter_element.innerHTML = response.count;
+        }
+    };
+
+    polling_xhttp.open('GET', '/api/v1/missers/count/', true);
+    polling_xhttp.send();
+};
+
+let post_misser = function() {
+    misser_xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 201) {
+            counter_element.innerHTML = parseInt(counter_element.innerHTML) + 1;
+            miss_it();
+        }
+    }
+
+    misser_xhttp.open('POST', '/api/v1/missers/', true);
+    misser_xhttp.setRequestHeader('X-CSRFToken', CSRF_TOKEN);
+    misser_xhttp.send()
+};
+
+let miss_it = function() {
+    cta_element.setAttribute('disabled', 'disabled')
+    cta_element.innerHTML = 'You miss it already!';
+    localStorage.setItem('misses', true);
+};
+
+window.setInterval(poll_count, 5000);
+
+if (localStorage.getItem('misses')) {
+    miss_it();
+}
+else {
+    cta_element.addEventListener('click', post_misser);
+}
