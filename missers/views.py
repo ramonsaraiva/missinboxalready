4b83d473fdcad579dba3d302c01aa1d774b10efd
Ramon Saraiva <ramonsaraiva@gmail.com>
2019-03-23 15:01:17 -0300

    Update view to process others (count less than 5% of the total)

--- missers/views.py
+++ missers/views.py
@@ -5,6 +5,7 @@ from rest_framework import (
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from django.db.models import Count
 from django.views.generic import ListView
 
 from .models import Misser
@@ -14,6 +15,33 @@ from .serializers import MisserSerializer
 class MissersView(ListView):
     model = Misser
     template_name = 'missers/missers.html'
+
+    def identify_others(self, countries, total, cap=0.05):
+        for name, count in countries.items():
+            if (count / total) < cap or name == 'Undefined':
+                yield (name, count)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        countries = self.get_queryset().values(
+            'country', 'country__name'
+        ).annotate(count=Count('country')).order_by('-count')
+
+        countries = {
+            country['country__name']: country['count']
+            for country in countries
+        }
+        total = len(self.object_list) - countries['Undefined']
+
+        others = list(self.identify_others(countries, total))
+        countries['Others'] = 0
+        for name, count in others:
+            del countries[name]
+            countries['Others'] += count
+
+        context['countries'] = countries
+        return context
 
 
 class MissersViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
